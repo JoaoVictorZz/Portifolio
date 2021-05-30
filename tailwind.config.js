16d9ee4cbdc006d64f3ed424a7f1dff8db396ad9
João Victor <joaovictordev207@gmail.com>
2021-05-30 20:27:18 -0300

    added file of config for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {,
+			'FiraCode':['Fira Code'],
+		}
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
