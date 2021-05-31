c282c7b10f194c8400c23346e47edf86c6f6461f
João Victor <joaovictordev207@gmail.com>
2021-05-31 14:19:08 -0300

    changed the home page location

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		fontFamily: {,
-			'FiraCode':['Fira Code'],
-		}
+		fontFamily: {
+			'FiraCode': ['Fira Code'],
+		},
 		extend: {},
 	},
 	variants: {
